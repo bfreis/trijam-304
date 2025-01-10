3d7cafef8085c236e3670dc0d9e984f560ae3254
Bruno Reis <bfreis@gmail.com>
2025-01-10 01:58:42 -0800

    add player position and draw the player

--- game/screen_maze.go
+++ game/screen_maze.go
@@ -15,22 +15,28 @@ const (
 )
 
 type MazeScreen struct {
-	maze *Maze
+	maze    *Maze
+	playerX int
+	playerY int
 }
 
 func NewMazeScreen() (*MazeScreen, error) {
 	const mazeString = `+--+--+--+
-|  |  |  |
-+--+--+--+
-|     |  |
-+--+--+--+
-|  |  |  |
+|  |  |   
++  +--+  +
+|        |
++  +  +--+
+|  |     |
 +--+--+--+`
 	maze, err := ParseMaze(mazeString)
 	if err != nil {
 		return nil, err
 	}
-	return &MazeScreen{maze: maze}, nil
+	return &MazeScreen{
+		maze:    maze,
+		playerX: 0,
+		playerY: 0,
+	}, nil
 }
 
 func (s *MazeScreen) Update(tick ebitenwrap.Tick) error {
@@ -53,6 +59,7 @@ func (s *MazeScreen) Draw(screen *ebiten.Image) {
 	offsetX := (float64(sw) - mazeWidth) / 2
 	offsetY := (float64(sh) - mazeHeight) / 2
 
+	// Draw maze walls
 	for y := 0; y < s.maze.Height; y++ {
 		for x := 0; x < s.maze.Width; x++ {
 			px := offsetX + float64(x)*cellWidth
@@ -72,4 +79,10 @@ func (s *MazeScreen) Draw(screen *ebiten.Image) {
 			}
 		}
 	}
+
+	// Draw player
+	playerRadius := float32(mazeCellDisplayWidth) / 4
+	playerPosX := float32(offsetX + float64(s.playerX)*cellWidth + cellWidth/2)
+	playerPosY := float32(offsetY + float64(s.playerY)*cellHeight + cellHeight/2)
+	vector.DrawFilledCircle(screen, playerPosX, playerPosY, playerRadius, color.RGBA{255, 200, 0, 255}, false)
 }
